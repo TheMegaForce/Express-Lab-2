465879cd9dab3fa88524ae092860c3260a27e90a
Ishan Azad <ishansazad@gmail.com>
2023-07-15 12:49:39 -0700

    Added Magic 8 Ball Route

--- server.js
+++ server.js
@@ -16,6 +16,33 @@ app.get("/tips/:total/:tipPercentage", (req, res) => {
 	});
 });
 
+app.get("/magic/:sentence", (req, res) => {
+	let ball = [
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",
+	];
+	let randBall = ball[Math.floor(Math.random() * ball.length)];
+	res.send(req.params.sentence + "?" + `<h1>` + randBall + `</h1>`);
+});
+
 app.listen(3000, () => {
 	console.log("listening");
 });
