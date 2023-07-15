10681483a1717f4d8b665b75faa05f51de0e6aa3
Ishan Azad <ishansazad@gmail.com>
2023-07-15 11:53:09 -0700

    Tip Calculator Route Completed

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.get("/greetings/:name", (req, res) => {
 	res.send("Hello " + req.params.name);
 });
 
+app.get("/tips/:total/:tipPercentage", (req, res) => {
+	let { total, tipPercentage } = req.params;
+
+	let answer = (parseInt(total) * parseInt(tipPercentage)) / 100;
+	res.send({
+		total: "$" + total,
+		tip_percentage: tipPercentage + "%",
+		tipped_ammount: "$" + answer,
+	});
+});
+
 app.listen(3000, () => {
 	console.log("listening");
 });
